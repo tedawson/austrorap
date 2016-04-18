599b0541853b9f882e3c67bd6cfe8fcb6eedee62
Ted Dawson <dawson.ted@gmail.com>
2016-04-18 21:06:54 +0200

    upload magistrix downloader

---
+++ magistrix.py
@@ -0,0 +1,35 @@
+#magistrix.de downloader
+from sys import argv
+from austrorap import *
+
+from sys import argv
+import austrorap2
+
+
+script, url, target = argv
+
+text_start = '<i class="icon-google-plus"></i>'
+text_end = "<p class='hidden-xs'>"
+lyrics = austrorap2.songtext(url, text_start, text_end)
+lyrics2 = unicode(lyrics, "utf-8")
+lyrics_list = lyrics2.split(' ')
+frequencies = austrorap2.ordered_freq_list(lyrics_list)
+unique_words, total_words, variety = austrorap2.lexical_variety(lyrics_list)
+
+f = open(target, 'w')
+
+f.write("Lyrics = \n" + lyrics + "\n")
+f.write("List of words = \n" + str(lyrics_list).decode("utf-8") + "\n")
+for pair in frequencies:
+    f.write(str(pair) + "\n")
+
+f.close()
+#unicode(bytestr, "utf-8")
+print "Words in Corpus: ",
+print total_words
+print "Unique Words in Corpus: ",
+print unique_words
+print "Lexical Variety in Corpus (Type-Token-Ratio): ",
+print variety
+print "Lyrics, word list, and frequencies saved as " + str(target)
+print lyrics_list
